4a0c5808006543b0b6a8c756c610ac11e3c9efd9
Nargys <32126532+nargysc@users.noreply.github.com>
2022-08-24 18:57:49 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+//2Write a function that takes two numbers (a and b) as Sum a and Return the result
+function sum(a, b) {
+   return a + b;
+}
+
+//Write a function that takes an object (a) and a string (b) as argument  Return true if the object has a property with key 'b' Return false otherwise
+const six = (object, n) => {
+    if (object[n]) {
+        return true;
+    } else {
+        return false;
+    }
+};
+console.log(sum);
+
+//Write a function that takes a number (a) as argument Split a into its individual digits and return them in an array.
+
+function funch(a)
+{
+return String(a).split("").map(Number);
+}
+
+//Write a function that takes a string as an argument. Extract the last 3 characters from the string. Return the result
+const string = (value) => {
+    return value.slice(-3);s
+};
+
+
+//Write a function that takes an object (a) and a string (b) as argument  Return true if the object has a property with key 'b' Return false otherwise
+const six = (object, n) => {
+    if (object[n]) {
+        return true;
+    } else {
+        return false;
+    }
+};
